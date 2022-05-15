de58b6585cb49bde7a8bf234d399ae7bad74ac13
YusufAliAnsar <102678112+YusufAliAnsar@users.noreply.github.com>
2022-05-15 21:17:49 +0530

    commit

--- Project/3_Implementation/inc/io.h
+++ Project/3_Implementation/inc/io.h
@@ -1,23 +1,25 @@
-#define START_ADC  ADCSRA |= (1<<ADSC)
-#define ENABLE_ADC ADCSRA |= (1<<ADEN)
+#define GPIOD_BASE_ADDR 0x40020C00	//Defining Base address for GPIO//for LESDs
+#define GPIOD_ODR_OFFSET 0x14       //LED are outputs so we are using ODR //Defining Offset Address for LEDs
+#define GPIOD_ODR *(volatile long *)(GPIOD_BASE_ADDR + GPIOD_ODR_OFFSET)
 
-#define SET_BIT(PORT,PIN)  PORT |= (1<<PIN)
-#define CLR_BIT(PORT,PIN)  PORT &= ~(1<<PIN)
+//Adding Base address and offset address for LEDs
 
-void ADC_init();
-uint16_t READ_ADC(uint8_t adcx);
-#define SET_PWM_VALUE(value)  OCR0A = value
+#define RCC_BASE_ADDR 0x40023800 //Defining RCC Address
+#define RCC_AHB1ENR_OFFSET 0x30   //Defining Offset for RCC
+#define RCC_AHB1ENR *(volatile long *)(RCC_BASE_ADDR + RCC_AHB1ENR_OFFSET)
 
-#define SET_BIT(PORT,PIN)  PORT |= (1<<PIN)
-#define CLR_BIT(PORT,PIN)  PORT &= ~(1<<PIN)
+//Adding Base address and offset address for RCC
 
-void PWM_init();
-#define START_TIMER TIMSK0|=(1<<TOIE0)
-#define STOP_TIMER  TIMSK0 &= ~(1<<TOIE0)
+#define GPIOA_BASE_ADDR 0x40020000  //Defining Base address for switch
+#define GPIOA_IDR_OFFSET 0x10 //switch is input so we are using IDR //Defining Offset of Switch
+#define GPIOA_IDR *(volatile long *)(GPIOA_BASE_ADDR + GPIOA_IDR_OFFSET)
 
-#define SET_BIT(PORT,PIN)  PORT |= (1<<PIN)
-#define CLR_BIT(PORT,PIN)  PORT &= ~(1<<PIN)
+//Adding Base address and offset address for SWITCH
 
-void TIMER_init();
-	
-}
+#define GPIOD_MODE_OFFSET 0x00
+#define GPIOD_MODE *(volatile long *)(GPIOD_BASE_ADDR + GPIOD_MODE_OFFSET)
+
+//Adding Base address and offset address To clear Previous for LEDs
+
+#define GPIOA_MODE_OFFSET 0x00
+#define GPIOA_MODE *(volatile long *)(GPIOA_BASE_ADDR + GPIOA_MODE_OFFSET)
