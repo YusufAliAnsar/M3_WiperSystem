375da6f065bd4ab54da2d2bd4c666a888b4722bc
YusufAliAnsar <102678112+YusufAliAnsar@users.noreply.github.com>
2022-05-15 21:15:30 +0530

    Update io.h

--- Project/3_Implementation/inc/io.h
+++ Project/3_Implementation/inc/io.h
@@ -6,3 +6,16 @@
 
 void ADC_init();
 uint16_t READ_ADC(uint8_t adcx);
+#define SET_PWM_VALUE(value)  OCR0A = value
+
+#define SET_BIT(PORT,PIN)  PORT |= (1<<PIN)
+#define CLR_BIT(PORT,PIN)  PORT &= ~(1<<PIN)
+
+void PWM_init();
+#define START_TIMER TIMSK0|=(1<<TOIE0)
+#define STOP_TIMER  TIMSK0 &= ~(1<<TOIE0)
+
+#define SET_BIT(PORT,PIN)  PORT |= (1<<PIN)
+#define CLR_BIT(PORT,PIN)  PORT &= ~(1<<PIN)
+
+void TIMER_init();
